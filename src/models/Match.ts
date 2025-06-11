0d5c503d00f6302cdf340c6f9e453dd272150049
virtualheet <heetvavadiya456@gmail.com>
2025-06-11 13:23:33 +0530

    feat(models): add Match model
    - Created Match schema
    - Defined required fields
    - Included timestamps
    - Added compatibility field
    - Implemented mongoose model

---
+++ src/models/Match.ts
@@ -0,0 +1,43 @@
+import type { IMatch } from "@/types";
+import mongoose, { Schema } from "mongoose";
+
+const MatchSchema = new Schema<IMatch>({
+    user1Id: {
+        type: String,
+        required: true,
+    },
+    user2Id: {
+        type: String,
+        required: true,
+    },
+    matchScore: {
+        type: Number,
+        required: true,
+    },
+    musicCompatibility: {
+        type: Schema.Types.Mixed,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    initiatedBy: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+    },
+    becameMutualAt: {
+        type: Date,
+        required: false,
+    },
+});
+
+export default mongoose.model<IMatch>("Match", MatchSchema);
