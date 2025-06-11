830bd1b34127bc24e8ecd4853d13b390d6736623
virtualheet <heetvavadiya456@gmail.com>
2025-06-11 13:28:28 +0530

    feat(models): add Message model
    - Created Message schema
    - Defined message attributes
    - Implemented mongoose model

---
+++ src/models/Message.ts
@@ -0,0 +1,59 @@
+import type { IMessage } from "@/types";
+import mongoose, { Schema } from "mongoose";
+
+const MessageSchema = new Schema<IMessage>({
+    conversationId: {
+        type: String,
+        required: true,
+    },
+    senderId: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    messageType: {
+        type: String,
+        required: true,
+    },
+    sharedContent: {
+        type: Schema.Types.Mixed,
+        required: false,
+    },
+    isRead: {
+        type: Boolean,
+        required: true,
+    },
+    readAt: {
+        type: Date,
+        required: false,
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+    },
+    deliveredAt: {
+        type: Date,
+        required: false,
+    },
+    sentAt: {
+        type: Date,
+        required: true,
+    },
+    editedAt: {
+        type: Date,
+        required: false,
+    },
+    deletedAt: {
+        type: Date,
+        required: false,
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+export default mongoose.model<IMessage>("Message", MessageSchema);  
