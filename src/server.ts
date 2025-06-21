302729363c204367b32296144ed4bd5a6a59c7f7
virtualheet <heetvavadiya456@gmail.com>
2025-06-22 03:05:59 +0530

    empty server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-
 import router from "./router/router";
 import { runServer } from './lib/db';
+import { createClient } from "redis";
+import cookieParser from 'cookie-parser';
 import { Server } from 'socket.io';
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server)
-
-app.use(express.json());
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000'],
-  credentials: true
-}));
-
-app.use(router);
 
 
 
