7614b9899427caea2fe3b45ab109d71e2779f329
virtualheet <heetvavadiya456@gmail.com>
2025-06-22 09:20:05 +0530

    feat(cloudinary): integrate cloudinary
    - Added cloudinary configuration
    - Created cloudinary lib file
    - Implemented cloudinary setup

--- src/functions/socket/sendMessage.ts
+++ src/functions/socket/sendMessage.ts
@@ -1,8 +1,20 @@
 import type { AuthRequest } from "@/middleware/auth";
+import cloudinary from "@/lib/cloudinary";
 
 export const sendMessage = async (req: AuthRequest, res: Response) => {
     try {
-        const { text, image } = req.body;
+        const { text, image } = req.body
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id.toString();
+        
+        let imageUrl = null;
+        if (image) {
+            const imageUrl = await cloudinary.uploader.upload(image, {
+                resource_type: 'image',
+                folder: 'messages',
+            });
+        }
+        
 
     } catch (error) {
         
