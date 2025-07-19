451248b3b8db28f2abe1fa8a90697a8f0408884a
virtualheet <heetvavadiya456@gmail.com>
2025-07-19 18:07:08 +0530

    country: {             type: String,         },

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,6 +27,7 @@ export interface IUser extends Document {
     
     location: {
         city: string;
+        country: string;
         coordinates: {
             lat: number;
             lng: number;
